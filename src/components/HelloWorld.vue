e28c84802b0c4348f758b3872f9fcb32099df16c
antonday07 <anhquangtong2000@gmail.com>
2024-08-15 00:04:45 +0700

    intinal vue base

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>hello world</h1>
+      <input type="text" v-model="message">
+      <div style="margin-top: 20px;">
+        {{ message }}
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: 'Adamlovecode'
+    }
+  },
+  created() {
+    console.log("hello thiss");
+  },
+  mounted() {
+    console.log("good job 12321321");
+  }
+}
+</script>
+<style lang="">
+  
+</style>
